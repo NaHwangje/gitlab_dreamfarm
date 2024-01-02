86a8b6ea47b69de2d0f9455d1138eac7b2316b89
Hwangje <na03266@naver.com>
2024-01-02 14:32:24 +0900

    유저 적용 완료

---
+++ android/app/src/main/kotlin/com/example/dream_farm_mqtt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dream_farm_mqtt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/mqtt/mqtt_server_client.dart
@@ -0,0 +1,186 @@
+/*
+ * Package : mqtt_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 31/05/2017
+ * Copyright :  S.Hamblett
+ */
+import 'dart:async';
+import 'dart:io';
+
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+final client = MqttServerClient('livewalk901.iptime.org', '1883');
+
+var pongCount = 0; // Pong counter
+
+Future<int> main() async {
+  /// 클라이언트 로그, 기본값은 false
+  client.logging(on: false);
+
+  /// mosquito용 mqtt 프로토콜
+  client.setProtocolV311();
+
+  /// Set secure working
+  // client.secure = true;
+
+  /// If you intend to use a keep alive you must set it here otherwise keep alive will be disabled.
+  client.keepAlivePeriod = 20;
+
+  /// The connection timeout period can be set if needed, the default is 5 seconds.
+  client.connectTimeoutPeriod = 2000; // milliseconds
+
+  /// Add the unsolicited disconnection callback
+  client.onDisconnected = onDisconnected;
+
+  /// Add the successful connection callback
+  client.onConnected = onConnected;
+
+  /// Add a subscribed callback, there is also an unsubscribed callback if you need it.
+  /// You can add these before connection or change them dynamically after connection if
+  /// you wish. There is also an onSubscribeFail callback for failed subscriptions, these
+  /// can fail either because you have tried to subscribe to an invalid topic or the broker
+  /// rejects the subscribe request.
+  client.onSubscribed = onSubscribed;
+
+  /// Set a ping received callback if needed, called whenever a ping response(pong) is received
+  /// from the broker.
+  client.pongCallback = pong;
+
+  /// Create a connection message to use or use the default one. The default one sets the
+  /// client identifier, any supplied username/password and clean session,
+  /// an example of a specific one below.
+  final connMess = MqttConnectMessage()
+      .withClientIdentifier('Hwangje')
+      .withWillTopic('willtopic') // If you set this you must set a will message
+      .withWillMessage('My Will message')
+      .startClean() // Non persistent session for testing
+      .withWillQos(MqttQos.atLeastOnce)
+      .authenticateAs('user', '1234');
+
+  print('Mosquitto client connecting....');
+  client.connectionMessage = connMess;
+
+  /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+  /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+  /// never send malformed messages.
+  try {
+    await client.connect();
+  } on NoConnectionException catch (e) {
+    // Raised by the client when connection fails.
+    print('client exception - $e');
+    client.disconnect();
+  } on SocketException catch (e) {
+    // Raised by the socket layer
+    print('socket exception - $e');
+    client.disconnect();
+  }
+
+  /// 연결 확인
+  if (client.connectionStatus!.state == MqttConnectionState.connected) {
+    print('Mosquitto client connected');
+  } else {
+    /// Use status here rather than state if you also want the broker return code.
+    print(
+        'ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+    client.disconnect();
+    exit(-1);
+  }
+
+  /// Ok, lets try a subscription
+  print('Subscribing to the test/lol topic');
+  const topic = 'test/lol'; // Not a wildcard topic
+  client.subscribe(topic, MqttQos.atMostOnce);
+
+  /// The client has a change notifier object(see the Observable class) which we then listen to to get
+  /// notifications of published updates to each subscribed topic.
+  client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+    final recMess = c![0].payload as MqttPublishMessage;
+    final pt =
+        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+    /// The above may seem a little convoluted for users only interested in the
+    /// payload, some users however may be interested in the received publish message,
+    /// lets not constrain ourselves yet until the package has been in the wild
+    /// for a while.
+    /// The payload is a byte buffer, this will be specific to the topic
+    print(
+        'Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+    print('');
+  });
+
+  /// If needed you can listen for published messages that have completed the publishing
+  /// handshake which is Qos dependant. Any message received on this stream has completed its
+  /// publishing handshake with the broker.
+  client.published!.listen((MqttPublishMessage message) {
+    print(
+        'Published notification:: topic is ${message.variableHeader!.topicName}, with Qos ${message.header!.qos}');
+  });
+
+  /// 토픽 설정 및 토픽 내용 입력
+  /// Lets publish to our topic
+  /// Use the payload builder rather than a raw buffer
+  /// Our known topic to publish to
+  const pubTopic = 'Dart/Mqtt_client/tea';
+  final builder = MqttClientPayloadBuilder();
+  builder.addString('whattjwasjdlaskdjfashjdkljfjahsdf');
+
+  /// Subscribe to it
+  print('Subscribing to the Dart/Mqtt_client/testtopic topic');
+  client.subscribe(pubTopic, MqttQos.exactlyOnce);
+
+  /// Publish it
+  print('Publishing our topic');
+  client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload!);
+
+  /// Ok, we will now sleep a while, in this gap you will see ping request/response
+  /// messages being exchanged by the keep alive mechanism.
+  print('Sleeping....');
+  await MqttUtilities.asyncSleep(60);
+
+  /// Finally, unsubscribe and exit gracefully
+  print('Unsubscribing');
+  client.unsubscribe(topic);
+
+  /// Wait for the unsubscribe message from the broker if you wish.
+  await MqttUtilities.asyncSleep(2);
+  print('Disconnecting');
+
+  client.disconnect();
+  print('Exiting normally');
+  return 0;
+}
+
+/// The subscribed callback
+void onSubscribed(String topic) {
+  print('Subscription confirmed for topic $topic');
+}
+
+/// The unsolicited disconnect callback
+void onDisconnected() {
+  print('OnDisconnected client callback - Client disconnection');
+  if (client.connectionStatus!.disconnectionOrigin ==
+      MqttDisconnectionOrigin.solicited) {
+    print('OnDisconnected callback is solicited, this is correct');
+  } else {
+    print(
+        'OnDisconnected callback is unsolicited or none, this is incorrect - exiting');
+    exit(-1);
+  }
+  if (pongCount == 3) {
+    print('Pong count is correct');
+  } else {
+    print('Pong count is incorrect, expected 3. actual $pongCount');
+  }
+}
+
+/// The successful connect callback
+void onConnected() {
+  print('OnConnected client callback - Client connection was successful');
+}
+
+/// Pong callback
+void pong() {
+  print('Ping response client callback invoked');
+  pongCount++;
+}
